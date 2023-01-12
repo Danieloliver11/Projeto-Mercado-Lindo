81961d4076f519c4e68d53a8660eb4cc5dc33bf1
Danieloliver11 <danielnatsu@hotmail.com>
2023-01-12 19:28:36 -0300

    inicializando o projeto

---
+++ src/main/java/com/mercadolindo/MercadoLindoApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolindo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoLindoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoLindoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadolindo/MercadoLindoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolindo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLindoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
